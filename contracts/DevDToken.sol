f2409413004063dd6557775e0c9897e29bfa3c3a
Demetrio <dema.atra1@gail.com>
2022-02-28 22:35:55 -0300

    transfer function | tests

--- contracts/DevDToken.sol
+++ contracts/DevDToken.sol
@@ -5,13 +5,29 @@ contract DevDToken {
 
     string public name = "DevD Token";
     string public symbol = "DEVD";
-    string public standard = "DevD Token v1.0";
     uint256 public totalSupply;
+    string public standard = "DevD Token v1.0";
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
     
     // Constructor
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        // Transfer event
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/DevDToken.js
+++ test/DevDToken.js
@@ -4,9 +4,11 @@ contract('DevDToken', function(accounts) {
 
     var tokenInstance;
 
+    // Tests
     it('initializes the contract with the correct values', function() {
         return DevDToken.deployed().then(function(instance) {
             tokenInstance = instance;
+            // Test
             return tokenInstance.name();
         }).then(function(name) {
             assert.equal(name, 'DevD Token', 'has the correct name');
@@ -22,6 +24,7 @@ contract('DevDToken', function(accounts) {
     it('allocates the initial supply upon deployment', function() {
         return DevDToken.deployed().then(function(instance) {
             tokenInstance = instance;
+            // Test
             return tokenInstance.totalSupply();
         }).then(function(totalSupply) {
             assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
@@ -30,4 +33,31 @@ contract('DevDToken', function(accounts) {
             assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the inicial supply to the admin account');
         });
     });
+
+    it('transfers token ownership', function() {
+        return DevDToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            // Test
+            return tokenInstance.transfer.call(accounts[1], 1000001);
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'error message must contains revert');
+            return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(success) {
+            assert.equal(success, true, 'it returns true')
+            return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+        }).then(function(receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer event"');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 250000, 'logs the transfer amount');
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(balance) {
+            assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+        });
+    });
+
 });
